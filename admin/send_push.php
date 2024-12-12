97c3a1c4f8f6d8fd9f585a2162cf596b21868e3e
kp-swazei <KanakParmar@swazei.com>
2024-12-12 18:54:11 +0530

    push notification fixed

--- admin/send_push.php
+++ admin/send_push.php
@@ -22,48 +22,61 @@ while($rw=mysqli_fetch_array($result))
 
 $qry_keys = "SELECT * FROM fcm_key";
 $rs_keys = mysqli_query($con,$qry_keys) or die("can not select ec");
+
 while($rw_keys=mysqli_fetch_array($rs_keys))
 {
+
+		
+					$appId = $rw_keys["topic_name"]; // Replace with your OneSignal App ID
+					$apiKey = $rw_keys["fcm_key"];    // Replace with your OneSignal REST API Key
+
+					$content = [
+						"en" => $Link_Desc
+					];
 				
-					$apiKey = $rw_keys["fcm_key"];
-					$topic =  "'".$rw_keys["topic_name"]."' in topics";
+					$headings = [
+						"en" => $Link_Title
+					];
+				
 					
-					//echo "<br>".$topic." ".$apiKey;
-					//exit;
 					
-					$url = 'https://fcm.googleapis.com/fcm/send';
-					$headers = array(
-						'Authorization: key=' . $apiKey,
-						'Content-Type: application/json'
-					);
-					
-					$notification_data = array(    //// when application open then post field 'data' parameter work so 'message' and 'body' key should have same text or value
-						'message' => $Link_Title
-					);
-			
-					$notification = array(       //// when application close then post field 'notification' parameter work
-						'body'  => $Link_Desc,
-						'sound' => 'default'
-					);
-			
-					$post = array(
-						'condition'         => $topic,
-						'notification'      => $notification,
-						"content_available" => true,
-						'priority'          => 'high',
-						'data'              => $notification_data
-					);
+					$fields = [
+						'app_id' => $appId,
+						'included_segments' => ['All'], // Target all users
+						'headings' => $headings,
+						'contents' => $content
+					];
+				
+					$fieldsJson = json_encode($fields);
+
 					
 				
 					$ch = curl_init();
-					curl_setopt($ch, CURLOPT_URL, $url);
-					curl_setopt($ch, CURLOPT_POST, true);
-					curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+					curl_setopt($ch, CURLOPT_URL, "https://api.onesignal.com/notifications");
+					curl_setopt($ch, CURLOPT_HTTPHEADER, [
+						'Content-Type: application/json; charset=utf-8',
+						'Authorization: Key ' . $apiKey
+					]);
 					curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-					curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post));
-					$result = curl_exec($ch);
-					print_r($result);
+					curl_setopt($ch, CURLOPT_POST, true);
+					curl_setopt($ch, CURLOPT_POSTFIELDS, $fieldsJson);
+					
+					// Debug the request
+					echo "Request Payload: " . $fieldsJson . "<br>";
+					
+					$response = curl_exec($ch);
+					
+					if ($response === FALSE) {
+						die('Curl failed: ' . curl_error($ch));
+					} else {
+						echo "Response: " . $response;
+					}
+					
 					curl_close($ch);
+					
+					
+
+
 				
 } 			
  
